4a431e2c5107249e64d6c9fa93465089d9cfcfab
KeithBautista <keith1829@gmail.com>
2023-02-15 20:24:08 +0000

    committing changes before revert

--- products/views.py
+++ products/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from .models import Product, Category
 from django.db.models.functions import Lower
 
 from .models import Product, Category
 from .forms import ProductForm
 
 # Create your views here.
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -59,12 +61,12 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show individual product details """
+    """A view to return a specific product selected"""
 
-    product = get_object_or_404(Product, pk=product_id)
+    products = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'product': product,
+        'product': products,
     }
 
     return render(request, 'products/product_detail.html', context)
@@ -72,17 +74,7 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
-        else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
-    else:
-        form = ProductForm()
-        
+    form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,

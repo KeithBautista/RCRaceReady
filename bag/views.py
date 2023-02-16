60fbd329cfd03ecd1eb86481b1379cabcd1955bb
KeithBautista <keith1829@gmail.com>
2023-02-16 04:32:49 +0000

    refactored python files

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (render, redirect,
+                              reverse, HttpResponse, get_object_or_404)
 from django.contrib import messages
 
 from products.models import Product
@@ -13,12 +14,14 @@ def remove_from_bag(request, item_id):
     try:
         bag = request.session.get('bag', {})
         bag.pop(item_id)
-        messages.success(request, f'{product.name} has been removed from your basket')
+        messages.success(request,
+                         f'{product.name} has been removed from your basket')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, f'Error removing this item from your basket {e}')
+        messages.error(request,
+                       f'Error removing this item from your basket {e}')
         return HttpResponse(status=500)
 
 
@@ -31,11 +34,13 @@ def adjust_bag(request, item_id):
 
     if quantity > 0:
         bag[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+        messages.success(request,
+                         f'Updated {product.name} quantity to {bag[item_id]}')
 
     else:
         bag.pop(item_id)
-        messages.success(request, f'{product.name} has been removed from your basket')
+        messages.success(request,
+                         f'{product.name} has been removed from your basket')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -43,7 +48,7 @@ def adjust_bag(request, item_id):
 
 def view_bag(request):
     """Renders Basket Cart"""
-    
+
     return render(request, 'bag/bag.html')
 
 
@@ -57,7 +62,8 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity  # increments the quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+        messages.success(request,
+                         f'Updated {product.name} quantity to {bag[item_id]}')
 
     else:
         bag[item_id] = quantity
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -7,7 +7,10 @@ from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
 
+
 @require_POST
+
+
 @csrf_exempt
 def webhook(request):
     """Listen for webhooks from Stripe"""
@@ -39,7 +42,8 @@ def webhook(request):
     # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed':
+        (handler.handle_payment_intent_payment_failed),
     }
 
     # Get the webhook type from Stripe

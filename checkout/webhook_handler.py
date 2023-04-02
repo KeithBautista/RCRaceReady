8b71240d8cc160bf710b1b7d64d8c1c59332446e
KeithBautista <keith1829@gmail.com>
2023-04-02 14:34:18 +0000

    email configuration set up

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -110,9 +110,10 @@ class StripeWH_Handler:
                 time.sleep(1)
         if order_exists:
             self._send_confirmation_email(order)
+            self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]}'
-                ' | SUCCESS: Verified order already in database',
+                content=f'Webhook received: {event["type"]} | \
+                    SUCCESS: Verified order already in database',
                 status=200)
         else:
             order = None
@@ -159,8 +160,8 @@ class StripeWH_Handler:
                     status=500)
         self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received:{event["type"]}'
-            '| SUCCESS: Created order in webhook',
+            content=f'Webhook received: {event["type"]} | \
+                SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):

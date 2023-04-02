6738927271f581b2207ecad421f31461f899360d
KeithBautista <keith1829@gmail.com>
2023-04-02 21:02:34 +0000

    redacted review model

--- products/models.py
+++ products/models.py
@@ -35,7 +35,7 @@ class Product(models.Model):
         return self.name
 
 
-class Review(models.Model):
+"""class Review(models.Model):
     product = models.ForeignKey(Product, related_name="reviews",
                                 on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
@@ -43,4 +43,4 @@ class Review(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return (self.product.name, self.name) """

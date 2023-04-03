40783f1064a3f6616fc1d7ea3bf1b394cdd5256a
KeithBautista <keith1829@gmail.com>
2023-04-03 07:15:48 +0000

    added contact us model and form

--- rcraceready/settings.py
+++ rcraceready/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'contactus',
 
     # Other Apps
     'crispy_forms',
--- rcraceready/urls.py
+++ rcraceready/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('contactus/', include('contactus.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "rcraceready.views.handle_not_found"
